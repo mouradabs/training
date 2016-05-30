1bb554b2df81b43b05aea9544465fbcc2a485073
James Halliday <mail@substack.net>
2016-05-30 10:17:27 +0100

    updated our server to use a stream

--- nearform/example/web/server.js
+++ nearform/example/web/server.js
@@ -4,6 +4,15 @@ var fs = require('fs')
 var server = http.createServer(function (req, res) {
   console.log(req.method, req.url)
   if (req.url === '/cool') {
+    var stream = fs.createReadStream('cool.txt')
+    stream.on('error', function (err) {
+      res.statusCode = 500
+      res.end('something went wrong: ' + err + '\n')
+    })
+    res.setHeader('content-type', 'text/html')
+    stream.pipe(res)
+
+    /*
     fs.readFile('cool.txt', function (err, src) {
       if (err) {
         res.statusCode = 500
@@ -13,6 +22,7 @@ var server = http.createServer(function (req, res) {
         res.end(src)
       }
     })
+    */
   } else {
     res.setHeader('content-type', 'text/html')
     res.end('<h1>hello</h1>\n')
