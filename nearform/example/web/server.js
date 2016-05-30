d0ae77823e21c117bea718bceb40d8a3004b18fd
James Halliday <mail@substack.net>
2016-05-30 10:21:00 +0100

    using ecstatic

--- nearform/example/web/server.js
+++ nearform/example/web/server.js
@@ -1,31 +1,13 @@
 var http = require('http')
 var fs = require('fs')
+var ecstatic = require('ecstatic')
+var st = ecstatic('public')
 
 var server = http.createServer(function (req, res) {
   console.log(req.method, req.url)
-  if (req.url === '/cool') {
-    var stream = fs.createReadStream('cool.txt')
-    stream.on('error', function (err) {
-      res.statusCode = 500
-      res.end('something went wrong: ' + err + '\n')
-    })
-    res.setHeader('content-type', 'text/html')
-    stream.pipe(res)
-
-    /*
-    fs.readFile('cool.txt', function (err, src) {
-      if (err) {
-        res.statusCode = 500
-        res.end('something went wrong: ' + err + '\n')
-      } else {
-        res.setHeader('content-type', 'text/html')
-        res.end(src)
-      }
-    })
-    */
-  } else {
+  if (req.url === '/hello') {
     res.setHeader('content-type', 'text/html')
     res.end('<h1>hello</h1>\n')
-  }
+  } else st(req, res)
 })
 server.listen(5000)
