c1047292dc3ddb66d49cc243aefec4488303ece3
James Halliday <mail@substack.net>
2016-05-30 10:13:41 +0100

    updated server.js with a route

--- nearform/example/web/server.js
+++ nearform/example/web/server.js
@@ -1,7 +1,21 @@
 var http = require('http')
+var fs = require('fs')
+
 var server = http.createServer(function (req, res) {
   console.log(req.method, req.url)
-  res.setHeader('content-type', 'text/html')
-  res.end('<h1>hello</h1>\n')
+  if (req.url === '/cool') {
+    fs.readFile('cool.txt', function (err, src) {
+      if (err) {
+        res.statusCode = 500
+        res.end('something went wrong: ' + err + '\n')
+      } else {
+        res.setHeader('content-type', 'text/html')
+        res.end(src)
+      }
+    })
+  } else {
+    res.setHeader('content-type', 'text/html')
+    res.end('<h1>hello</h1>\n')
+  }
 })
 server.listen(5000)
