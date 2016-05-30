f0deb9a30dafbb4ac60c7d09dd03916f04966f73
James Halliday <mail@substack.net>
2016-05-30 12:03:16 +0100

    chat client

--- nearform/example/chat/main.js
+++ nearform/example/chat/main.js
@@ -6,7 +6,7 @@ var wsock = require('websocket-stream')
 var stream = wsock('ws://' + location.host)
 
 var root = document.querySelector('#content')
-var state = { date: null }
+var state = { lines: [] }
 function update () {
   html.update(root, render(state))
 }
@@ -14,12 +14,25 @@ update()
 
 function render (state) {
   return html`<div>
-    <h1>the time</h1>
-    <div>${state.date ? strftime('%T', state.date) : ''}</div>
+    <h1>chat</h1>
+    <form onsubmit=${onsubmit}>
+      <input type="text" name="msg">
+    </form>
+    <div>
+      ${state.lines.map(function (line) {
+        return html`<div>${line}</div>`
+      })}
+    </div>
   </div>`
+  function onsubmit (ev) {
+    ev.preventDefault()
+    var msg = this.elements.msg.value
+    stream.write(msg + '\n')
+    this.reset()
+  }
 }
 
 stream.pipe(split()).on('data', function (buf) {
-  state.date = new Date(Number(buf.toString()))
+  state.lines.push(buf.toString())
   update()
 })
