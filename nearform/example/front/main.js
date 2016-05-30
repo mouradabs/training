16194efc44b9ad380cff6d8f38c35a6817e325ef
James Halliday <mail@substack.net>
2016-05-30 11:34:24 +0100

    frontend using yo-yo

--- nearform/example/front/main.js
+++ nearform/example/front/main.js
@@ -3,9 +3,19 @@ var strftime = require('strftime')
 var fs = require('fs')
 var formatString = fs.readFileSync('format.txt', 'utf8')
 
+var html = require('yo-yo')
+var root = document.querySelector('#content')
+
+function render (date) {
+  return html`<div>
+    <h1>hello</h1>
+    <div>${strftime(formatString, date)}</div>
+  </div>`
+}
+
 setInterval(function () {
   xhr('/time', function (err, res, body) {
     var d = new Date(Number(body))
-    document.body.innerText = strftime(formatString, d)
+    html.update(root, render(d))
   })
 }, 1000)
