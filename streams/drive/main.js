d32a7f11ee241a5d4b5ae0a3351066b88d95f7e6
James Halliday <mail@substack.net>
2016-06-06 17:06:54 +0300

    working hyperdrive

---
+++ streams/drive/main.js
@@ -0,0 +1,56 @@
+var hyperdrive = require('hyperdrive')
+var level = require('level-browserify')
+var sub = require('subleveldown')
+var collect = require('collect-stream')
+var concat = require('concat-stream')
+var db = level('drive2.db')
+var info = sub(db, 'info')
+var html = require('yo-yo')
+var root = document.querySelector('#content')
+
+var state = { files: [] }
+var archive
+update()
+
+info.get('link', function (err, link) {
+  var drive = hyperdrive(sub(db, 'drive'))
+  if (link) link = Buffer(link, 'hex')
+  archive = drive.createArchive(link, { live: true })
+  if (!link) info.put('link', archive.key.toString('hex'))
+  list()
+})
+
+function list () {
+  collect(archive.list(), function (err, files) {
+    console.log('files=', files)
+    state.files = files
+    update()
+  })
+}
+
+function update () {
+  html.update(root, html`<div>
+    <form onsubmit=${onsubmit}>
+      <input type="text" name="filename">
+      <div><textarea name="contents"></textarea></div>
+    </form>
+    ${state.files.map(function (file) {
+      return html`<div><a onclick=${show}>${file.name}</a></div>`
+      function show () {
+        archive.createFileReadStream(file.name)
+          .pipe(concat(function (body) {
+            console.log('body=', body)
+            location.href = 'data:text/plain,' + body
+          }))
+      }
+    })}
+  </div>`)
+
+  function onsubmit (ev) {
+    ev.preventDefault()
+    var file = this.elements.filename.value
+    var stream = archive.createFileWriteStream(file)
+    stream.on('finish', list)
+    stream.end(this.elements.contents.value)
+  }
+}
