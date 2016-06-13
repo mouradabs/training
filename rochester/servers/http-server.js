508033b65e9ca205838889d9f2d0b921b2b37c1a
James Halliday <mail@substack.net>
2016-06-13 11:50:35 -0400

    http server

---
+++ rochester/servers/http-server.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var server = http.createServer(function (req, res) {
+  // req is a readable stream for POST, PUT methods
+  // res is a writable stream
+  console.log(req.method, req.url)
+  //console.log(req.headers)
+
+  res.statusCode = 200
+  res.setHeader('content-type', 'text/html')
+  res.end('<h1>some html</h1>\n')
+})
+server.listen(5002)
