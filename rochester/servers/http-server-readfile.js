46e153a6ea40e49885be1af61e93d337aac9b179
James Halliday <mail@substack.net>
2016-06-13 11:55:38 -0400

    http server readfile

--- rochester/servers/http-server-readfile.js
+++ rochester/servers/http-server-readfile.js
@@ -1,14 +1,17 @@
 var http = require('http')
 var fs = require('fs')
-var server = http.createServer(opts, function (req, res) {
+var server = http.createServer(function (req, res) {
   // req is a readable stream for POST, PUT methods
   // res is a writable stream
   console.log(req.method, req.url)
   //console.log(req.headers)
 
-  res.statusCode = 200
   res.setHeader('content-type', 'text/html')
   fs.readFile('file.html', function (err, src) {
-  res.end('<h1>some html</h1>\n')
+    if (err) {
+      res.statusCode = 500
+      res.end(err + '\n')
+    } else res.end(src)
+  })
 })
 server.listen(5002)
