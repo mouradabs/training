391c83781f219d79193e3b4a017f7b5da0bb18a5
James Halliday <mail@substack.net>
2016-06-13 13:59:54 -0400

    a feed of messages

--- rochester/wsock/server.js
+++ rochester/wsock/server.js
@@ -2,13 +2,16 @@ var http = require('http')
 var path = require('path')
 var ecstatic = require('ecstatic')
 var st = ecstatic(path.join(__dirname, 'public'))
+var onend = require('end-of-stream')
 
 var server = http.createServer(st)
 server.listen(5000)
 
 var wsock = require('websocket-stream')
 wsock.createServer({ server: server }, function (stream) {
-  stream.write('{"n":5}\n')
-  stream.write('{"n":6}\n')
-  stream.write('{"n":10}\n')
+  var iv = setInterval(function () {
+    stream.write(JSON.stringify({ n: Math.random() }) + '\n')
+  }, 1000)
+
+  onend(stream, function () { clearInterval(iv) })
 })
